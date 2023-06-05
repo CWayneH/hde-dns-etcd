ad2e8cd3548d90a741f35922a115d93c7a400ebd
Kaiwang11 <bear6548@gmail.com>
2023-06-05 14:04:01 +0800

    add host table controller

--- host_table_controller/push_data.py
+++ host_table_controller/push_data.py
@@ -20,24 +20,24 @@ def home():
        print("sucessed")
        return jsonify("x" )
 
-
 # input: {ip: ,count: ,priority: }
 @app.route("/refresh")
 def refresh():
-#      response = requests.get('http://127.0.0.1:5001/fake_client')
-     new_dict=response.json()
-     with open("table.json") as f :
-        host_table=json.load(f)
-        for k in new_dict.keys():
-                for ip,new_ip in zip(host_table[k],new_dict[k]):
-                        ip["count"]=new_ip["count"]
-                        ip["priority"]=new_ip["priority"]
-        host_table[k].sort(key=lambda x : x['priority'])      
-        json.dump(host_table,f)
+        
+        response = requests.get('http://127.0.0.1:3001/hdSimu')
+        print(response.content)
+#      new_dict=response.json()
+#      with open("table.json") as f :
+#         host_table=json.load(f)
+#         for k in new_dict.keys():
+#                 host_table[k]=new_dict[k]
+#         host_table[k].sort(key=lambda x : x['priority'])      
+#         json.dump(host_table,f)
 
         return "refresh sucessed"
 
 if __name__ == "__main__":
-        response = requests.get('http://127.0.0.1:5001/fake_client')
-        print(response)
+        response = requests.get('http://127.0.0.1:3001/hdSimu/ini')
+        print()
+        print(response.content)
         app.run(port=8001,debug=True)
