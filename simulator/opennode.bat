9189a82c1904c30993b523b1a64dfa2577035a25
CWayneH <90815681+CWayneH@users.noreply.github.com>
2023-06-08 00:38:25 +0800

    Update opennode.bat

--- simulator/opennode.bat
+++ simulator/opennode.bat
@@ -1,14 +1,16 @@
 @ECHO off
 rem ECHO "add ipv4 into interface"
-rem FOR /L %A IN (100,10,120) DO netsh interface ipv4 add address "區域連線* 2" 192.168.137.%A 255.255.255.0
+rem FOR /L %A IN (170,10,190) DO netsh interface ipv4 add address "乙太網路" 192.168.0.%A 255.255.255.0
 ECHO "move to working directory"
 PAUSE
-cd C:\Users\Wayne-NB\Desktop\MASTER.InService\DistributedSystem\etcd_data\simulator
-call conda activate etcd_data
+rem pass working path to %1
+rem "D:\Wayne\DSys\etcd"
+cd %1
+call conda activate dsys_lab
 ECHO "start host node.."
-start "collector" node host-data-collector.js 192.168.137.100
-start "host1" node host-data.js 192.168.137.110 30001 192.168.137.100
-start "host2" node host-data.js 192.168.137.120 30001 192.168.137.100
+start "collector" node host-data-collector.js 192.168.0.170
+start "host1" node host-data.js 192.168.0.180 30001 192.168.0.170
+start "host2" node host-data.js 192.168.0.190 30001 192.168.0.170
 ECHO "Prepare to kill host process"
 PAUSE
 taskkill /FI "WindowTitle eq host1" /T /F
@@ -18,6 +20,6 @@ PAUSE
 taskkill /FI "WindowTitle eq collector" /T /F
 rem ECHO "delete ipv4 from interface"
 PAUSE
-rem FOR /L %A IN (100,10,120) DO netsh interface ipv4 delete address "區域連線* 2" 192.168.137.%A
+rem FOR /L %A IN (170,10,190) DO netsh interface ipv4 delete address "乙太網路" 192.168.0.%A
 ECHO "done."
 PAUSE
