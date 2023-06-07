f3bac81e89b6a8317a9e317b06dd4f846132eadc
CWayneH <90815681+CWayneH@users.noreply.github.com>
2023-06-07 19:35:22 +0800

    Update client request.ps1

--- simulator/client request.ps1
+++ simulator/client request.ps1
@@ -1,5 +1,6 @@
 # http://127.0.0.1:30001/hdSimu/ASI/index.html?perf=2
-$url = "http://127.0.0.1:30001/hdSimu/"
+$url1 = "http://192.168.137.110:30001/hdSimu/"
+$url2 = "http://192.168.137.120:30001/hdSimu/"
 $r1 = "USA/"
 $r2 = "EUR/"
 $r3 = "ASI/"
@@ -8,7 +9,9 @@ $srv2 = "app"
 $pf1 = "?perf=0"
 $pf2 = "?perf=1"
 $pf3 = "?perf=2"
+<#
 while ($true) {
+	$url = $url1, $url2 | Get-Random;
 	$r = $r1, $r2, $r3 | Get-Random;
 	$srv = $srv1, $srv2 | Get-Random;
 	$pf = $pf1, $pf2, $pf3 | Get-Random;
@@ -24,3 +27,13 @@ while ($true) {
 		return $false
 	}
 }
+#>
+for($i=0;$i-lt10;$i++){
+	# totally GET method in simplify
+	$pf = $pf1, $pf2, $pf3 | Get-Random;
+	$srv = $srv1, $srv2 | Get-Random;
+	$r = $r1, $r2, $r3 | Get-Random;
+	$url = $url1, $url2 | Get-Random;
+	$data = Invoke-RestMethod $url$r$srv$pf;
+	Write-Host $data
+	}
