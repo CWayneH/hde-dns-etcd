58906cc73b8a242b623f124068444983471cbf58
CWayneH <90815681+CWayneH@users.noreply.github.com>
2023-06-08 18:13:47 +0800

    Update client request.ps1

--- simulator/client request.ps1
+++ simulator/client request.ps1
@@ -1,6 +1,13 @@
 # http://127.0.0.1:30001/hdSimu/ASI/index.html?perf=2
-$url1 = "http://192.168.137.110:30001/hdSimu/"
-$url2 = "http://192.168.137.120:30001/hdSimu/"
+$url1 = "http://192.168.137.180:30001/hdSimu/"
+$url2 = "http://192.168.137.190:30001/hdSimu/"
+$url3 = "http://192.168.137.200:30001/hdSimu/"
+$url4 = "http://192.168.137.210:30001/hdSimu/"
+$url5 = "http://192.168.137.220:30001/hdSimu/"
+$url6 = "http://192.168.137.230:30001/hdSimu/"
+$url7 = "http://192.168.137.240:30001/hdSimu/"
+$url8 = "http://192.168.137.250:30001/hdSimu/"
+$url = $url1, $url2, $url3, $url4, $url5, $url6, $url7, $url8
 $r1 = "USA/"
 $r2 = "EUR/"
 $r3 = "ASI/"
@@ -35,6 +42,6 @@ for($i=0;$i-lt10;$i++){
 	$srv = $srv1, $srv2, $srv3 | Get-Random;
 	$r = $r1, $r2, $r3 | Get-Random;
 	$url = $url1, $url2 | Get-Random;
-	$data = Invoke-RestMethod $url$r$srv$pf;
+	$data = Invoke-RestMethod ($url | Get-Random)$r$srv$pf;
 	Write-Host $data
 	}
