d4231e6b08524359f34b56868cbce875ef3b00ed
Kaiwang11 <bear6548@gmail.com>
2023-06-10 16:04:59 +0800

    modify host table reading

--- host_table_controller/push_data.py
+++ host_table_controller/push_data.py
@@ -12,51 +12,21 @@ import time
 import pandas as pd 
 
 
-host_table=pd.read_csv("outcome.csv")
+# host_table=pd.read_csv("../data engine/outcome.csv")
 
 app = Flask(__name__)
 
-@app.route("/")
 def push_data():
-    
-    return hosts
-    
-    
-    
+#     print(host_table)
+    host_table=pd.read_csv("../data engine/outcome.csv")
+    host_dict={}
+    for index, row in host_table.iterrows():  
+          host_dict[row['host']]=row['priIP']
+    response = requests.post('http://127.0.0.1:3001/',data=host_dict)
+    return host_dict
 
-# name = "example.com"
-
-# # # refresh the priority
-# # #example:  new_dict ={"example":{"ip":192.1.1.1,"count": 10,"priority":1} }
-# @app.route("/push_data/<name>")
-# def push_data(name):
-#        print(name)
-#        with open("table.json") as f :
-#               host_table=json.load(f)
-#               print(host_table[name][0])
-#               return  host_table[name]
-       
-# @app.route("/")
-# def home():
-#        print("sucessed")
-#        return jsonify("x" )
-
-# # input: {ip: ,count: ,priority: }
-# @app.route("/update")
-# def refresh():
-#         response = requests.get('http://127.0.0.1:3001/hdSimu')
-#         print(response.content)
-# #      new_dict=response.json()
-# #      with open("table.json") as f :
-# #         host_table=json.load(f)
-# #         for k in new_dict.keys():
-# #                 host_table[k]=new_dict[k]
-# #         host_table[k].sort(key=lambda x : x['priority'])      
-# #         json.dump(host_table,f)
-
-#         return "refresh sucessed"
-
-# if __name__ == "__main__":
-#         response = requests.get('http://127.0.0.1:3001/hdSimu/ini')
-#         print(response.content)
-#         app.run(port=8001,debug=True)
+if __name__ == "__main__":
+#        while True:
+#              time.sleep(5)
+        k=push_data()
+        print(k)
