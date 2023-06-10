6fa7531e8c039c7ba667316d4daeb4dfde1546c0
Kaiwang11 <bear6548@gmail.com>
2023-06-10 19:54:25 +0800

    add random drop

--- host_table_controller/push_data.py
+++ host_table_controller/push_data.py
@@ -10,23 +10,38 @@ from firebase_admin import db,firestore
 import os
 import time
 import pandas as pd 
+import random
 
-
-# host_table=pd.read_csv("../data engine/outcome.csv")
 
 app = Flask(__name__)
 
+def rand_pick():
+      host_table=pd.read_csv("../data engine/outcome.csv")
+      host_table[['rbstatus']]=0
+      dead_one=random.randint(0,host_table.shape[0]-1)
+      for index, row in host_table.iterrows(): 
+        #     print(row)
+            if index==dead_one:
+                  host_table.at[index, 'rbstatus'] = 1
+                  print("drop index ",index, row )
+      print(host_table)
+      host_table.to_csv("../data engine/outcome.csv",index = False )
+
 def push_data():
-#     print(host_table)
     host_table=pd.read_csv("../data engine/outcome.csv")
+#     print(host_table.shape)
     host_dict={}
-    for index, row in host_table.iterrows():  
-          host_dict[row['host']]=row['priIP']
-    response = requests.post('http://127.0.0.1:3001/',data=host_dict)
+    for index, row in host_table.iterrows(): 
+          if row['rbstatus']==0:
+                host_dict[row['host']]=row['priIP']
+    print(len(host_dict))
+    response = requests.post('http://127.0.0.1:3001/test',data=host_dict,params=host_dict)
+    print(response.text)
     return host_dict
 
 if __name__ == "__main__":
-#        while True:
-#              time.sleep(5)
-        k=push_data()
-        print(k)
+       while True:
+             time.sleep(5)
+             rand_pick()
+             k=push_data()
+             print(k)
